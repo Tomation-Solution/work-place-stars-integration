1bd2893273019c514d0675f0ab381ccf191b4bc3
tim1119 <timothyinioluwa@gmail.com>
2023-09-26 20:27:03 +0100

    fix: updated hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,7 +3,7 @@
 const Hero = () => {
   return (
     <div className="font-poppins bg-gradient-to-br from-[#0d5574] via-[#337ba0] to-[#569fcb] p-4 h-screen w-full flex items-center  ">
-      <div className="place-items-center   w-full h-full grid grid-cols-1 ">
+      <div className="  w-full h-full flex flex-column items-center ">
       
         <div className="xl:col-span-2 ">
           <h3 className="text-white text-center md:text-left xs:text-red-400 text-[30px] leading-[50px]  sm:text-[38px] sm:leading-[63px] md:text-[45px] md:leading-[60px]  lg:text-[55px] lg:leading-[70px] xl:text-[58px] font-bold leading-1 ml-5   xl:leading-[80px]">
